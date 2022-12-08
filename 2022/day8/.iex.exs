2ff6f26ed1c73c2dbacccc2eb733588c6038e6ab
Trevor Stone <github@trevorstone.org>
2022-12-08 00:05:20 -0700

    2020 day 8 in Elixir

---
+++ 2022/day8/.iex.exs
@@ -0,0 +1,7 @@
+import_file("../.iex.exs")
+c("day8.exs")
+daymod = Day8
+exampletxt = "input.example.txt"
+actualtxt = "input.actual.txt"
+IO.puts("Run on #{exampletxt}: Runner.run(Day8, exampletxt)")
+IO.puts("Run on #{actualtxt}: Runner.run(Day8, actualtxt)")
