594281ac117ace1b036094b049287ac9ff2565af
Trevor Stone <tstone@google.com>
2022-12-19 18:33:30 -0700

    Further state graph reduction and cleanup for 2022 day 19

--- 2022/day19/day19.exs
+++ 2022/day19/day19.exs
@@ -17,6 +17,7 @@ defmodule Day19 do
   of resource each minute, starting one minute after creation.  Problems focus
   on the maximum number of geodes a blueprint can produce.
   """
+
   defmodule State do
     defstruct robots: %{ore: 1, clay: 0, obsidian: 0, geode: 0},
               resources: %{ore: 0, clay: 0, obsidian: 0, geode: 0}
@@ -32,81 +33,53 @@ defmodule Day19 do
 
     def can_build?(state, cost), do: Enum.all?(cost, fn {k, v} -> v <= state.resources[k] end)
 
-    def add_resources(state, res) do
-      struct!(state,
-        resources: Map.merge(state.resources, res, fn _k, v1, v2 -> v1 + v2 end)
-      )
-    end
+    def add_resources(state, res),
+      do: struct!(state, resources: Map.merge(state.resources, res, fn _k, v1, v2 -> v1 + v2 end))
 
     def all_possible(state, blueprint, time) do
-      [
-        state
-        | [:geode, :obsidian, :ore, :clay]
-          |> Enum.filter(&can_build?(state, blueprint[&1]))
-          |> Enum.filter(&worthwhile(state, blueprint, time, &1))
-          |> Enum.take(2)
-          |> Enum.map(&build(state, &1, blueprint[&1]))
-      ]
+      options =
+        [:geode, :obsidian, :ore, :clay]
+        |> Enum.filter(&can_build?(state, blueprint[&1]))
+        |> Enum.filter(&worthwhile(state, blueprint, time, &1))
+        |> Enum.take(2)
+
+      built = options |> Enum.map(&build(state, &1, blueprint[&1]))
+      if :geode in options or :obsidian in options, do: built, else: built ++ [state]
     end
 
     def worthwhile(_, _, time, :geode), do: time > 1
 
     def worthwhile(_, _, time, :obsidian) when time <= 2, do: false
 
-    def worthwhile(state, blueprint, time, :obsidian) do
-      expensive = blueprint.geode.obsidian
-      state.resources.obsidian + state.robots.obsidian * (time - 2) < (time - 1) * expensive
-    end
+    def worthwhile(%State{resources: res, robots: robots}, %{max_cost: worst}, time, :obsidian),
+      do: res.obsidian + robots.obsidian * (time - 2) < (time - 1) * worst.obsidian
 
     def worthwhile(_, _, time, :ore) when time <= 2, do: false
 
-    def worthwhile(state, blueprint, time, :ore) do
-      expensive = Enum.max([blueprint.geode.ore, blueprint.obsidian.ore, blueprint.clay.ore])
-      state.resources.ore + state.robots.ore * (time - 2) < (time - 1) * expensive
-    end
+    def worthwhile(%State{resources: res, robots: robots}, %{max_cost: worst}, time, :ore),
+      do: res.ore + robots.ore * (time - 2) < (time - 1) * worst.ore
 
     def worthwhile(_, _, time, :clay) when time <= 3, do: false
 
-    def worthwhile(state, blueprint, time, :clay) do
-      expensive = blueprint.obsidian.clay
-      state.resources.clay + state.robots.clay * (time - 3) < (time - 2) * expensive
-    end
+    def worthwhile(%State{resources: res, robots: robots}, %{max_cost: worst}, time, :clay),
+      do: res.clay + robots.clay * (time - 3) < (time - 2) * worst.clay
 
-    def discard_unnecessary(state, bp, time) do
+    def discard_unnecessary(%State{resources: res} = state, %{max_cost: worst}, time) do
       struct!(state,
         resources: %{
-          ore:
-            min(
-              state.resources.ore,
-              Enum.max([bp.geode.ore, bp.obsidian.ore, bp.clay.ore, bp.ore.ore]) * (time - 1)
-            ),
-          clay: min(state.resources.clay, bp.obsidian.clay * (time - 1)),
-          obsidian: min(state.resources.obsidian, bp.geode.obsidian * (time - 1)),
-          geode: state.resources.geode
+          ore: min(res.ore, worst.ore * (time - 1)),
+          clay: min(res.clay, worst.clay * (time - 2)),
+          obsidian: min(res.obsidian, worst.obsidian * (time - 1)),
+          geode: res.geode
         }
       )
-    end
-
-    def sorter(a, b) do
-      [al, bl] =
-        Enum.map([a, b], fn x ->
-          [x.robots.geode, x.robots.obsidian, x.robots.clay, x.robots.ore]
-        end)
-
-      al >= bl
     end
   end
 
   @doc "Multiply blueprint IDs by max geode count over 24 minutes, sum results."
   def part1(input) do
-    blueprints = Enum.map(input, &parse_line/1)
-
-    Enum.map(blueprints, fn bp ->
-      Task.async(fn ->
-        IO.puts(:stderr, "#{inspect(self())} checking blueprint #{inspect(bp)}")
-        {bp.id, best_score(%State{}, bp, 24)}
-      end)
-    end)
+    Enum.map(input, &parse_line/1)
+    |> Enum.map(fn bp -> Task.async(fn -> {bp.id, best_score(%State{}, bp, 24)} end) end)
     |> Task.await_many(:infinity)
     |> Enum.map(fn {id, score} -> id * score end)
     |> Enum.sum()
@@ -114,14 +87,8 @@ defmodule Day19 do
 
   @doc "Multiply the max geode count of the first 3 blueprints over 32 minutes."
   def part2(input) do
-    blueprints = Enum.map(Enum.take(input, 3), &parse_line/1)
-
-    Enum.map(blueprints, fn bp ->
-      Task.async(fn ->
-        IO.puts(:stderr, "#{inspect(self())} checking blueprint #{inspect(bp)}")
-        best_score(%State{}, bp, 32)
-      end)
-    end)
+    Enum.map(Enum.take(input, 3), &parse_line/1)
+    |> Enum.map(fn bp -> Task.async(fn -> best_score(%State{}, bp, 32) end) end)
     |> Task.await_many(:infinity)
     |> Enum.product()
   end
@@ -131,20 +98,22 @@ defmodule Day19 do
     stats = :ets.new(String.to_atom("stats_#{bp.id}"), [:set])
     :ets.update_counter(stats, :hits, 0, {:hits, 0})
     :ets.update_counter(stats, :misses, 0, {:misses, 0})
-    {best, path} = best_score(state, bp, time, cache, stats)
-    IO.puts(:stderr, "Blueprint #{bp.id} got #{best} #{inspect(self())}")
+    best = best_score(state, bp, time, cache, stats)
 
     [hits, misses] =
       Enum.map([:hits, :misses], fn key -> Keyword.fetch!(:ets.lookup(stats, key), key) end)
 
-    IO.puts(:stderr, "Cache hits: #{hits} misses: #{misses} #{inspect(self())}")
+    IO.puts(
+      :stderr,
+      "Blueprint #{bp.id} cache hits: #{hits} misses: #{misses} #{inspect(self())}"
+    )
+
     :ets.delete(cache)
     :ets.delete(stats)
-    for {state, i} <- Enum.with_index(path, 1), do: IO.puts(:stderr, "Step #{i}: #{inspect(state)}")
     best
   end
 
-  defp best_score(state, _bp, 0, _cache, _stats), do: {State.score(state), []}
+  defp best_score(state, _bp, 0, _cache, _stats), do: State.score(state)
 
   defp best_score(state, bp, time, cache, stats) do
     cache_key = {State.discard_unnecessary(state, bp, time), time}
@@ -167,11 +136,8 @@ defmodule Day19 do
 
         State.all_possible(state, bp, time)
         |> Enum.map(&State.add_resources(&1, to_add))
-        |> Enum.map(fn x ->
-          {best, path} = best_score(x, bp, time - 1, cache, stats)
-          {best, [x | path]}
-        end)
-        |> Enum.max_by(fn {score, _} -> score end)
+        |> Enum.map(fn x -> best_score(x, bp, time - 1, cache, stats) end)
+        |> Enum.max()
         |> tap(fn best -> :ets.insert(cache, {cache_key, best}) end)
     end
   end
@@ -183,13 +149,21 @@ defmodule Day19 do
       |> Enum.drop(1)
       |> Enum.map(&String.to_integer/1)
 
-    %{
+    prepare_blueprint(%{
       id: id,
       ore: %{ore: ore_cost},
       clay: %{ore: clay_cost},
       obsidian: %{ore: obsidian_ore, clay: obsidian_clay},
       geode: %{ore: geode_ore, obsidian: geode_obsidian}
-    }
+    })
+  end
+
+  defp prepare_blueprint(bp) do
+    Map.put(bp, :max_cost, %{
+      ore: Enum.max([bp.ore.ore, bp.clay.ore, bp.obsidian.ore, bp.geode.ore]),
+      clay: bp.obsidian.clay,
+      obsidian: bp.geode.obsidian
+    })
   end
 
   def main() do
