33293ed043382c7432c4a4073aa116c8aded5f08
Trevor Stone <tstone@google.com>
2023-12-07 19:20:17 -0700

    2023 day 7: compute hand value in constructor, >10x speedup

--- 2023/day7/Day7.jl
+++ 2023/day7/Day7.jl
@@ -7,69 +7,56 @@
 
 """# Advent of Code 2023 day 7
 [Read the puzzle](https://adventofcode.com/2023/day/7)
+
+Input is poker-style hands expressed as five characters 2-9, T, J, Q, K.
+Hands are ranked as five-of-a-kind, four-of-a-kind, full-house, three-of-a-kind,
+two-pair, one-pair, high-card.  When comparing to hands of the same type,
+compare cards in hand order (not "highest card in the hand" but "higher first
+card").  Each hand also has a bid, output is sum(rank * bid) with the wors hand
+having rank 1.  Part 2 changes J to a joker which can have any value when
+determining type but has a lower value than all other cards when breaking ties.
 """
 module Day7
 
-struct Card
-  value::Char
-end
-
 const ORDER = "023456789TJQKA"
-function Base.isless(a::Card, b::Card)
-  isless(findfirst(a.value, ORDER), findfirst(b.value, ORDER))
-end
 
 struct Hand
-  cards::Vector{Card}
+  cards::AbstractString
   bid::Int
-  jokers::Bool
-end
-
-function handtype(h::Hand)
-  cards = h.jokers ? optimize_jokers(h.cards) : h.cards
-  sort([count(==(i), cards) for i in unique(cards)]; rev=true) |> filter(!=(1))
-end
-
-function Base.isless(a::Hand, b::Hand)
-  haa = handtype(a)
-  hab = handtype(b)
-  if haa == hab
-    if a.jokers
-      replace(a.cards, Card('J') => Card('0')) < replace(b.cards, Card('J') => Card('0'))
-    else
-      a.cards < b.cards
-    end
-  else
-    haa < hab
+  value::Tuple{Vector{Int}, Vector{Int}}
+  function Hand(cards, bid, jokers)
+    cards = jokers ? replace(cards, 'J' => '0') : cards
+    mod = jokers ? optimize_jokers(cards) : cards
+    handtype = sort([count(==(i), mod) for i in unique(mod)]; rev=true)
+    tiebreaker = collect(findfirst(c, ORDER) for c in cards)
+    new(cards, bid, (handtype, tiebreaker))
   end
 end
 
+Base.isless(a::Hand, b::Hand) = a.value < b.value
+
 function optimize_jokers(cards)
-  jokers = count(==(Card('J')), cards)
+  jokers = count(==('0'), cards)
   if jokers == 5
     return cards
   end
-  nonjokers = [(count(==(i), cards), i) for i in filter(!=(Card('J')), cards)]
-  most = first(sort(nonjokers, rev=true))
-  replace(cards, Card('J') => most[2])
+  nonjokers = [(count(==(i), cards), i) for i in filter(!=('0'), cards)]
+  _, most = first(sort(nonjokers, rev=true))
+  replace(cards, '0' => most)
 end
 
-function part1(lines)
-  input = parseinput(lines, false)
-  sortedhands = sort(input)
-  sum(map(ib -> first(ib) * last(ib), enumerate(map(h -> h.bid, sortedhands))))
-end
+part1(lines) = solution(lines, false)
+part2(lines) = solution(lines, true)
 
-function part2(lines)
-  input = parseinput(lines, true)
-  sortedhands = sort(input)
+function solution(lines, jokers)
+  sortedhands = sort(parseinput(lines, jokers))
   sum(map(ib -> first(ib) * last(ib), enumerate(map(h -> h.bid, sortedhands))))
 end
 
 function parseinput(lines, jokers)
   map(lines) do line
     cards, bid = split(line)
-    Hand([Card(c) for c in cards], parse(Int, bid), jokers)
+    Hand(cards, parse(Int, bid), jokers)
   end
 end
 
