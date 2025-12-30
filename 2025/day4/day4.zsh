5d40d8d45ffc97d6f6e3deb1dd4f0e1ab6499b60
Trevor Stone <github@trevorstone.org>
2025-12-29 20:11:43 -0700

    BSD and GNU cut differ in whitespace handling

--- 2025/day4/day4.zsh
+++ 2025/day4/day4.zsh
@@ -8,8 +8,8 @@
 # Advent of Code 2025 day 4
 # Read the puzzle at https://adventofcode.com/2025/day/4
 # Input is a grid of . (empty) and @ (obstacle) characters.  An obstacle can be
-# moved if there fewer than 4 obstacles in the 8 adjacent spaces; objects at the
-# edge can be considered to have empty space "off the grid."
+# moved if there are fewer than 4 obstacles in the 8 adjacent spaces;
+# objects at the edge can be considered to have empty space "off the grid."
 # Part 1 is the number of obstacles that can be removed from the initial state.
 # Part 2 is the number of total obstacles that can be removed if you remove each
 # obstacle and then look for more removable obstacles.
@@ -31,7 +31,7 @@ function solve {
   PART2=TODO
   input=$(cat $inputfile)
   ((lnum=0))
-  maxlines=$(wc -l <<< $input | cut -f 1 -d' ')
+  maxlines=$(wc -l <<< $input | sed 's/^ *//')
   while IFS='' read -r line ; do
     ((lnum++))
     for ((i=1; i <= $#line; i++)) do
